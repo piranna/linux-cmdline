32e5b2fdd4ed35404f72ef178194462ee3c581eb
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-09-27 07:50:17 +0200

    Added test

---
+++ test.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const deepStrictEqual = require('assert').deepStrictEqual
+
+const linuxCmdline = require('.')
+
+
+const cmdline = 'initrd=\\init-lin.img root=UUID=uuid someflag a.b=c d.e f=g,h'
+const expected =
+{
+  initrd: '\\init-lin.img',
+  root: 'UUID=uuid',
+  someflag: true,
+  a: { b: 'c' },
+  d: { e: true },
+  f: [ 'g', 'h' ]
+}
+
+var result = linuxCmdline(cmdline)
+
+deepStrictEqual(result, expected)
