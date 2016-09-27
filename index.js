338c1364c076b71e3410de6193553fabcbd1a967
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-09-27 07:23:47 +0200

    Original source code from `nodeos-mount-filesystems` module

---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * This functions takes the `cmdline` from `/proc/cmdline` **showed below in
+ * the example** and splits it into key/value pairs
+ * @access private
+ * @param  {String} cmdline This string contains information about the
+ *                          initrd and the root partition
+ * @return {Object}         It returns a object containing key/value pairs
+ *                          if there is no value for the key then its just true.
+ *                          **For more Information, look at the example**
+ * @example
+ *   var cmdline1 = 'initrd=\\initramfs-linux.img root=PARTUUID=someuuidhere\n'
+ *   var cmdline2 = 'somevar root=PARTUUID=someuuidhere\n'
+ *
+ * 	 var res1 = linuxCmdline(cmdline1)
+ * 	 var res2 = linuxCmdline(cmdline2)
+ * 	 console.log(res1)
+ * 	 //-> { initrd: '\\initramfs-linux.img',root: 'PARTUUID=someuuidhere' }
+ * 	 console.log(res2)
+ * 	 //-> { somevar: true, root: 'PARTUUID=someuuidhere' }
+ */
+function linuxCmdline(cmdline)
+{
+  var result = {}
+
+  cmdline.trim().split(' ').forEach(function(arg)
+  {
+    arg = arg.split('=')
+
+    var key = arg.shift()
+    var val = true
+
+    if(arg.length)
+    {
+      val = arg.join('=').split(',')
+      if(val.length === 1) val = val[0]
+    }
+
+    result[key] = val
+  })
+
+  return result
+}
+
+
+module.exports = linuxCmdline
